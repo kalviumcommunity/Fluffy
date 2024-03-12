d5a7aed662ff4b29f1779929c1a28d0e9e2ba0da
Snegan P <pssj2914@gmail.com>
2024-03-12 17:56:54 +0530

    Created a new branch GET-api.

---
+++ Frontend/client/src/HomeComponents/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <div className='bac'></div>
+      <div className='b2'></div>
+      <div className='b3'></div>
+      <div className='b4'></div>
+    </>
+  );
+}
+
+export default Home;
