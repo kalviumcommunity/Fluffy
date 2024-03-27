b883178a7fe207690ac21d7e59fac02d2b012a5b
Snegan P <pssj2914@gmail.com>
2024-03-27 16:52:29 +0530

    Modified home component [27/03/2024]

--- Frontend/client/src/HomeComponents/Home.jsx
+++ Frontend/client/src/HomeComponents/Home.jsx
@@ -1,93 +1,64 @@
-import React from 'react';
-import Navbar from './Navbar';
-import AdoptingImage from '../images/dog.webp';
-import PetFoodsImage from '../images/adopt1.webp';
-import PetToysImage from '../images/adopt2.webp';
-import LostAndFoundImage from '../images/food.png';
+// Home.js
+import React from "react";
+import { Parallax } from "react-parallax";
+import Navbar from "./Navbar";
+
+import Pet from '../images/adopt2.webp'
+import Pet2 from '../images/adopt1.webp'
 
 function Home() {
   return (
     <>
       <Navbar />
-      <div style={{ marginTop: '20px' }}>
-        <div className="background" style={{ 
-          backgroundImage: `url(${AdoptingImage})`, 
-          height: '80vh', 
-          display: 'flex', 
-          alignItems: 'center', 
-          justifyContent: 'center', 
-          color: '#fff', 
-          textAlign: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          backgroundSize: 'cover'
-        }}>
-          <div>
-            <h1 style={{ fontSize: '2.5rem', marginBottom: '20px' }}>Introducing Pets for Adoption</h1>
-            <h2 style={{ fontSize: '1.5rem' }}>Find Your Furry Companion</h2>
+      <Parallax className="image" bgImage={Pet} strength={800}>
+      <div className='content'>
+        <section style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <h2 style={{ marginLeft: '250px', fontSize: '4em', color: '#fff' }}>Awesome Parallax Scrolling</h2>
+          <div style={{position:"relative", width: '1000px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <div style={{ position: 'relative', width: '900px', height: '500px', overflow: 'hidden', boxShadow: '0 25px 35px rgba(0,0,0,0.1)' }}>
+              <img src={Pet2} alt="" style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', objectFit: 'cover', borderRadius: '10px' }} />
+            </div>
+            <div style={{ position: 'absolute', right: '-80px', marginTop: '400px', marginBottom: '100px', padding: '30px', background: '#333', zIndex: 1, maxWidth: '500px', color: 'white', boxShadow: '0 25px 35px rgba(0,0,0,0.1)', borderBottom: '6px solid #4ac7ff', borderRadius: '10px' }}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequuntur, numquam error suscipit tempora hic aut,
+                excepturi expedita libero eaque rem quam fugiat saepe
+                aliquid illum inventore voluptate fugit eos eveniet! Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Culpa
+                eius at unde, non doloremque corrupti ut veniam ipsa
+                tempora excepturi voluptates provident harum asperiores
+                vero dolores placeat vitae distinctio illum?
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="demo-gap" style={{ marginTop: '50px' }}>
-          <h3 style={{ textAlign: 'center', fontSize: '2rem' }}>Fluffy's Pet Foods</h3>
-        </div>
-        <div className="background" style={{ 
-          backgroundImage: `url(${PetFoodsImage})`, 
-          height: '80vh', 
-          display: 'flex', 
-          alignItems: 'center', 
-          justifyContent: 'center', 
-          color: '#fff', 
-          textAlign: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          backgroundSize: 'cover'
-        }}>
-          <div>
-            <h1 style={{ fontSize: '2.5rem', marginBottom: '20px' }}>Explore Fluffy's Pet Foods</h1>
-            <h2 style={{ fontSize: '1.5rem' }}>Healthy Options for Your Furry Friend</h2>
-          </div>
-        </div>
-        <div className="demo-gap" style={{ marginTop: '50px' }}>
-          <h3 style={{ textAlign: 'center', fontSize: '2rem' }}>Fluffy's Pet Toys</h3>
-        </div>
-        <div className="background" style={{ 
-          backgroundImage: `url(${PetToysImage})`, 
-          height: '80vh', 
-          display: 'flex', 
-          alignItems: 'center', 
-          justifyContent: 'center', 
-          color: '#fff', 
-          textAlign: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          backgroundSize: 'cover'
-        }}>
-          <div>
-            <h1 style={{ fontSize: '2.5rem', marginBottom: '20px' }}>Discover Fluffy's Pet Toys</h1>
-            <h2 style={{ fontSize: '1.5rem' }}>Keep Your Pet Entertained and Happy</h2>
-          </div>
-        </div>
-        <div className="demo-gap" style={{ marginTop: '50px' }}>
-          <h3 style={{ textAlign: 'center', fontSize: '2rem' }}>Lost and Found</h3>
-        </div>
-        <div className="background" style={{ 
-          backgroundImage: `url(${LostAndFoundImage})`, 
-          height: '80vh', 
-          display: 'flex', 
-          alignItems: 'center', 
-          justifyContent: 'center', 
-          color: '#fff', 
-          textAlign: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          backgroundSize: 'cover'
-        }}>
-          <div>
-            <h1 style={{ fontSize: '2.5rem', marginBottom: '20px' }}>Fluffy's Lost and Found</h1>
-            <h2 style={{ fontSize: '1.5rem' }}>Reunite with Your Beloved Pet</h2>
-          </div>
-        </div>
+        </section>
       </div>
+    </Parallax>
+    <Parallax className="image" bgImage={Pet2} strength={800}>
+      <div className='content'>
+        <section style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <h2 style={{ marginLeft: '250px', fontSize: '4em', color: '#fff' }}>Awesome Parallax Scrolling</h2>
+          <div style={{position:"relative", width: '1000px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <div style={{ position: 'relative', width: '900px', height: '500px', overflow: 'hidden', boxShadow: '0 25px 35px rgba(0,0,0,0.1)' }}>
+              <img src={Pet} alt="" style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', objectFit: 'cover', borderRadius: '10px' }} />
+            </div>
+            <div style={{ position: 'absolute', right: '-80px', marginTop: '400px', marginBottom: '100px', padding: '30px', background: '#333', zIndex: 1, maxWidth: '500px', color: 'white', boxShadow: '0 25px 35px rgba(0,0,0,0.1)', borderBottom: '6px solid #4ac7ff', borderRadius: '10px' }}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequuntur, numquam error suscipit tempora hic aut,
+                excepturi expedita libero eaque rem quam fugiat saepe
+                aliquid illum inventore voluptate fugit eos eveniet! Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Culpa
+                eius at unde, non doloremque corrupti ut veniam ipsa
+                tempora excepturi voluptates provident harum asperiores
+                vero dolores placeat vitae distinctio illum?
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Parallax>
+      
     </>
   );
 }
